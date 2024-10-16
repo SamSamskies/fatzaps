da752a57f2306c99daa724e56fb5be66f67e6893
Sam Samskies <samsamskies@strike.me>
2024-10-15 19:41:22 -0500

    convert app to deno

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # fatzaps
 Currently, this is just a script that finds recent fat zaps on Nostr and prints out the top results.
 
+## Prerequisites
+
+Deno 2.0 - Visit https://deno.com for installation instructions.
+
 ## Usage
 
-1. Install the dependencies using npm or yarn or whatever your heart desires
-1. Run the script `npm start`
+Run the script `deno task start`
--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,30 @@
-const { useWebSocketImplementation, Relay } = require("nostr-tools/relay");
-const {
-  npubEncode,
-  noteEncode,
-  naddrEncode,
-  neventEncode,
-} = require("nostr-tools/nip19");
-const lightBolt11Decoder = require("light-bolt11-decoder");
+import { Relay } from "nostr-tools/relay";
+import { npubEncode, neventEncode, noteEncode, naddrEncode } from "nostr-tools/nip19";
+import { Event } from "nostr-tools/pure";
+import { associateBy } from "@std/collections/associate-by";
+import lightBolt11Decoder from 'light-bolt11-decoder'
 
-useWebSocketImplementation(require("ws"));
-
-const relayUri = process.argv[2]
-  ? `wss://${process.argv[2]}`
+const relayUri = Deno.args[0]
+  ? `wss://${Deno.args[0]}`
   : "wss://relay.nostr.band";
 
-const getTag = (event, tag) => event.tags.find((t) => t[0] === tag);
+const getTag = (event: Event, tag: string) => event.tags.find((t) => t[0] === tag);
 
-const extractAmountInSats = (invoice) => {
+const extractAmountInSats = (invoice: string) => {
   try {
-    return (
-      lightBolt11Decoder
+    const sections = associateBy(lightBolt11Decoder
         .decode(invoice)
-        .sections.find(({ name }) => name === "amount").value / 1000
-    );
+        .sections, ({ name }) => name)
+    const amountSection = sections['amount'] as { name: "amount"; letters: string; value: string }
+
+    return Number(amountSection.value) / 1000
   } catch (err) {
     console.error(err);
     return 0;
   }
 };
 
-const getZappedEventNip19Id = (zapReceiptEvent) => {
+const getZappedEventNip19Id = (zapReceiptEvent: Event) => {
   try {
     const eTag = getTag(zapReceiptEvent, "e");
 
@@ -36,13 +32,13 @@ const getZappedEventNip19Id = (zapReceiptEvent) => {
       return noteEncode(eTag[1]);
     } else {
       const aTag = getTag(zapReceiptEvent, "a");
-      const [kind, pubkey, identifier] = aTag[1].split(":");
-      const recommendedRelay = aTag[2] ?? relayUri;
+      const [kind, pubkey, identifier] = aTag?.[1].split(":") ?? [];
+      const recommendedRelay = aTag?.[2] ?? relayUri;
 
       return naddrEncode({
         pubkey,
         identifier,
-        kind,
+        kind: Number(kind),
         relays: [recommendedRelay],
       });
     }
@@ -53,9 +49,9 @@ const getZappedEventNip19Id = (zapReceiptEvent) => {
   }
 };
 
-const getZapEvent = (event) => {
+const getZapEvent = (event: Event) => {
   try {
-    return JSON.parse(getTag(event, "description")[1]);
+    return JSON.parse(getTag(event, "description")?.[1] ?? "");
   } catch (err) {
     console.error(err);
     console.log(event);
@@ -63,17 +59,17 @@ const getZapEvent = (event) => {
   }
 };
 
-const getZapperPubkey = (zapReceiptEvent) => {
+const getZapperPubkey = (zapReceiptEvent: Event) => {
   const zapEvent = getZapEvent(zapReceiptEvent);
 
   return zapEvent ? zapEvent.pubkey : null;
 };
 
-const normalizeZapReceiptEvents = (zapReceiptEvents) => {
+const normalizeZapReceiptEvents = (zapReceiptEvents: Event[]) => {
   return zapReceiptEvents.map((event) => {
     const zapEvent = getZapEvent(event);
     const zapperNpub = zapEvent ? npubEncode(zapEvent.pubkey) : null;
-    const zapAmount = extractAmountInSats(getTag(event, "bolt11")[1]);
+    const zapAmount = extractAmountInSats(getTag(event, "bolt11")?.[1] ?? '');
     const comment = zapEvent?.content;
     const zappedNip19Id = getZappedEventNip19Id(event);
     const isAnonZap = zapEvent ? Boolean(getTag(zapEvent, "anon")) : false;
@@ -92,7 +88,7 @@ const normalizeZapReceiptEvents = (zapReceiptEvents) => {
 const start = async () => {
   const relay = await Relay.connect(relayUri);
   const numberOfEvents = 10;
-  const zapReceiptEvents = [];
+  const zapReceiptEvents: Event[] = [];
 
   const sub = relay.subscribe(
     [
@@ -104,20 +100,16 @@ const start = async () => {
     {
       onevent(event) {
         const zapperPubkey = getZapperPubkey(event);
-        const isSelfZap = zapperPubkey === getTag(event, "p")[1];
+        const isSelfZap = zapperPubkey === getTag(event, "p")?.[1];
 
         if (!isSelfZap && (getTag(event, "a") || getTag(event, "e"))) {
           zapReceiptEvents.push(event);
         }
       },
       oneose() {
-        sub.close();
-        relay.close();
-
         const results = normalizeZapReceiptEvents(zapReceiptEvents);
 
         results.sort((a, b) => a.zapAmount - b.zapAmount);
-
         results
           .filter(
             ({ zapperNpub, zappedNip19Id }) =>
@@ -138,7 +130,7 @@ const start = async () => {
                 : `nostr:${zapperNpub}`;
               const normalizedComment =
                 comment.length === 0 ? comment : `"${comment}"\n\n`;
-              const normalizedLink = zappedNip19Id.startsWith("naddr1")
+              const normalizedLink = zappedNip19Id?.startsWith("naddr1")
                 ? `https://njump.me/${zappedNip19Id}`
                 : `nostr:${zappedNip19Id}`;
               console.log(zapReceiptId);
@@ -148,6 +140,7 @@ const start = async () => {
               );
             },
           );
+        Deno.exit(0);
       },
     },
   );
